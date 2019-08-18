a24f743fefef11228ddb4c8a0cc0e660cf2f2ca0
Evgeniy Konstantinov <Konstantinov-91@mail.ru>
2019-08-18 12:49:05 +0300

    testing_init: some settings to testing, fix core and shared module. Core and shared now have one instance in all application.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -6,6 +6,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { environment } from '@atmEnvironments/environment';
+
 import { rootReducers } from './store/root.reducers';
 
 @NgModule({
@@ -17,4 +18,16 @@ import { rootReducers } from './store/root.reducers';
     !environment.production ? StoreDevtoolsModule.instrument() : []
   ]
 })
-export class CoreModule {}
+export class CoreModule {
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule
+    };
+  }
+
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error('StateModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-export class SharedModule { }
+export class SharedModule {
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule
+    };
+  }
+
+  constructor(@Optional() @SkipSelf() parentModule?: SharedModule) {
+    if (parentModule) {
+      throw new Error('StateModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
